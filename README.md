56649744e514b37690006eed1150cd0a87f540d7
Matthew Marchbanks <mattamarchbanks@gmail.com>
2019-04-13 23:07:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# socialMediaAPIWeek1" 
---
+++ src/main/java/com/promineomatt/socialMediaApi/App.java
@@ -0,0 +1,15 @@
+package com.promineomatt.socialMediaApi;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan("com.promineotech.socialMediaApi")
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
